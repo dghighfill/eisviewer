90dff4050f87bc39ee56a08b38fc643063421517
Dale Highfill <dghighfill@dstsystems.com>
2014-02-20 22:00:40 -0600

    Beefing up the UI

--- web-app/app/js/eis.js
+++ web-app/app/js/eis.js
@@ -1,10 +1,17 @@
-var myApp = angular.module('myApp',[]);
+var app = angular.module("myApp", []);
 
-myApp.controller("EisController", ['$scope', function($scope) {
- 
-    $scope.name = "Dale Highfill";
+function EisController($scope){
+    $scope.templateCriteria = ['<event>',
+     	                      '   <header>',
+                               '      <eventType></eventType>',
+                               '      <eventName></eventName>',
+                               '      <eventSource></eventSource>',
+                               '   </header>',
+                               '</event>'].join('\n');
+    
     $scope.init = function() {
-        alert( "Hello " + $scope.name );
+        console.log("EisController initialization");
+        $scope.criteria = $scope.templateCriteria;
     };
     
     $scope.resetTimer = function() {
@@ -12,18 +19,60 @@ myApp.controller("EisController", ['$scope', function($scope) {
     };
     
     $scope.loadTemplate = function(){
-    	$scope.txtCriteria = templateCriteria;
+    	$scope.criteria = $scope.templateCriteria;
+    };
+};
+
+
+function TimerController($scope, $timeout) {
+	$scope.hour = 0;
+	$scope.minutes = 0;
+	$scope.seconds = 0;
+	
+    $scope.timer = "00:00:00"; 
+  
+    $scope.start = function(){
+    	$scope.hour = 0;
+    	$scope.minutes = 0;
+    	$scope.seconds = 0;
+    	$scope.timer = "00:00:00";
+    	$timeout(countUp, 1000);
     }
     
-    $scope.templateCriteria = ['<event>',
-    	                      '   <header>',
-                              '      <eventType></eventType>',
-                              '      <eventName></eventName>',
-                              '      <eventSource></eventSource>',
-                              '   </header>',
-                              '</event>'].join('\n');
-}]);
-// onclick="onSubscribe()"
-//function onSubscribe(){
-//	alert("Hello World");
-//}
+    var countUp = function() {
+        $scope.seconds++;
+        $timeout(countUp, 1000);
+        display();
+    };
+
+    var display = function(){
+    	if( $scope.seconds == 5 ){
+    		rollIt();
+    	}
+    	$scope.timer = formatTimer();
+    };
+
+    var format = function( n ){
+    	// Pad a leading zero if it's less than 10.
+    	return (n < 10) ? '0' + n : n;
+    };
+    var rollIt = function(){
+    	console.debug( "rollIt "+$scope.seconds);
+    	$scope.seconds = 0
+    	if( $scope.minutes == 60 ){
+    		$scope.hour++;
+    		$scope.minutes = 0;
+    	}
+    	else
+    	{
+    		$scope.minutes++;
+    	}
+    	console.debug( "rollIt "+$scope.seconds);
+    };
+    
+    var formatTimer = function(){
+    	return format($scope.hour)+":"+format($scope.minutes)+":"+format($scope.seconds);
+    };
+    $timeout(countUp, 1000);
+
+};
